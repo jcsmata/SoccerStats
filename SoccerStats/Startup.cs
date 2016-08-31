52e3528a451497e354a2cc459cd943c42b872b5d
João Mata <joaomata@gmail.com>
2016-09-01 00:09:50 +0100

    Add project files.

---
+++ SoccerStats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoccerStats.Startup))]
+namespace SoccerStats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
